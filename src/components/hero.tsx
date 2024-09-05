4fd661b9e81926dd52a0fa5c2cff101ac9a89411
Saidkodirov <stolibjon123@gmail.com>
2024-09-06 02:23:56 +0500

    fx

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
       <p className="mb-8 leading-relaxed">Chillwave portland ugh, knausgaard fam polaroid iPhone. Man braid swag typewriter affogato, hella selvage wolf narwhal dreamcatcher.</p>
       <div className="flex w-full md:justify-start justify-center items-end">
         <div className="relative mr-4 lg:w-full xl:w-1/2 w-2/4">
-          <label for="hero-field" className="leading-7 text-sm text-gray-600">Placeholder</label>
+          <label htmlFor="hero-field" className="leading-7 text-sm text-gray-600">Placeholder</label>
           <input type="text" id="hero-field" name="hero-field" className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:ring-2 focus:ring-blue-200 focus:bg-transparent focus:border-blue-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
         </div>
         <button className="inline-flex text-white bg-blue-500 border-0 py-2 px-6 focus:outline-none hover:bg-blue-600 rounded text-lg">Button</button>
